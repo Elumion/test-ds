a1fb1166fd40453426f5df589da88d311059af21
Elumion <dovgalyukvlad87@gmail.com>
2025-07-26 00:56:40 +0300

    create logic for changing slides and initializing slider

--- dev/src/section-slider-pin-points.js
+++ dev/src/section-slider-pin-points.js
@@ -6,8 +6,48 @@ if (!customElements.get("s-slider-pin-points")) {
         super();
       }
 
-      connectedCallback() {}
-      disconnectedCallback() {}
+      connectedCallback() {
+        this.swiperEl = this.querySelector(".js-slider-pin-points__slider");
+        this.addEventListener("click", this.handleClick);
+        setTimeout(this.initSlider, 0);
+      }
+      disconnectedCallback() {
+        this.removeEventListener("click", this.handleClick);
+        this.swiperEl?.swiper?.destroy(true, true);
+      }
+
+      handleClick = (event) => {
+        event.preventDefault();
+        event.stopPropagation();
+        const target = event.target;
+        if (target.closest(".js-slider-pin-points__slider-btn-next")) {
+          this.swiperEl?.swiper?.slideNext(300);
+        } else if (target.closest(".js-slider-pin-points__slider-btn-prev")) {
+          this.swiperEl?.swiper?.slidePrev(300);
+        } else if (target.closest(".js-slider-pin-points__click-item")) {
+          const activeButtonIndex = target.closest(".js-slider-pin-points__click-item").dataset.index;
+          this.swiperEl?.hasAttribute("data-loop") ? this.swiperEl?.swiper?.slideToLoop(+activeButtonIndex, 300) : this.swiperEl?.swiper?.slideTo(+activeButtonIndex, 300);
+        }
+      };
+      initSlider = () => {
+        const swiperParams = {
+          autoplay: this.swiperEl?.hasAttribute("data-autoplay"),
+          loop: this.swiperEl?.hasAttribute("data-loop"),
+          navigation: false,
+        };
+
+        Object.assign(this.swiperEl, swiperParams);
+
+        this.swiperEl.initialize();
+        this.swiperEl?.swiper?.slideTo(0, 300);
+        this.swiperEl?.swiper?.on("slideChange", this.handleSlideChange);
+      };
+
+      handleSlideChange = (swiper) => {
+        const newActiveBtn = this.querySelector(`.js-slider-pin-points__click-item[data-index="${swiper.realIndex}"]`);
+        this.querySelector(".js-slider-pin-points__click-item.is-active")?.classList.remove("is-active");
+        newActiveBtn?.classList.add("is-active");
+      };
     }
   );
 }
